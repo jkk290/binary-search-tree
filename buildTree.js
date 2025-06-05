66cb100c3fce44dd0b0b693779e559b0ee27ffb8
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-06-05 13:31:51 -1000

    Add mergeSort function to sort arrays for buildTree function

---
+++ buildTree.js
@@ -0,0 +1,7 @@
+import { Node } from './node.js';
+import { Tree } from './tree.js';
+import { mergeSort } from './mergeSort.js'
+
+export default function buildTree(array) {
+
+};
---
+++ mergeSort.js
@@ -0,0 +1,47 @@
+export default function mergeSort(array){
+    if (array.length === 1) {
+        return array;
+    } else {
+        let midPoint = Math.floor(array.length / 2);
+        let leftHalf = array.slice(0, midPoint);
+        let rightHalf = array.slice(midPoint);
+
+        let sortedLeft = mergeSort(leftHalf);
+        let sortedRight = mergeSort(rightHalf);
+
+        return merge(sortedLeft, sortedRight);
+    }
+
+}
+
+function merge(sortedLeft, sortedRight) {
+    let sortedArray = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while ((leftIndex < sortedLeft.length) && (rightIndex < sortedRight.length)) {
+        if (sortedLeft[leftIndex] < (sortedRight[rightIndex])) {
+            sortedArray.push(sortedLeft[leftIndex]);
+            leftIndex++;
+        } else if (sortedLeft[leftIndex] > (sortedRight[rightIndex])) {
+            sortedArray.push(sortedRight[rightIndex]);
+            rightIndex++;
+        } else if (sortedLeft[leftIndex] === (sortedRight[rightIndex])) {
+            sortedArray.push(sortedLeft[leftIndex]);
+            leftIndex++;
+            sortedArray.push(sortedRight[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    
+    if (leftIndex < sortedLeft.length) {
+        sortedArray = sortedArray.concat(sortedLeft.slice(leftIndex));
+    }
+    if (rightIndex < sortedRight.length) {
+        sortedArray = sortedArray.concat(sortedRight.slice(rightIndex));
+    }
+
+    return sortedArray;
+
+};
