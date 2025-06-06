56fab1e87341e2ae1ca1e2279925a431146e8d3d
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-06-05 14:32:00 -1000

    Start implementing recursive helper function for buildTree

--- buildTree.js
+++ buildTree.js
@@ -9,3 +9,14 @@ export function buildTree(array) {
     let uniqueArray = removeDuplicates(sortedArray);
 
 };
+
+
+function recursiveBuilder(array, startIndex, endIndex) {
+    if (startIndex < endIndex) {
+        return null;
+    }
+
+    let midPoint = Math.floor(array.length / 2);
+
+    let newNode = new Node(midPoint);
+}
